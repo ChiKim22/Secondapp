f92179c16c02b45166cc94bed87d00f472a7694a
ChiKim22 <wlsgh2505@gmail.com>
2021-09-26 20:21:58 +0900

    Laravel

--- resources/views/bbs/index.blade.php
+++ resources/views/bbs/index.blade.php
@@ -1,7 +1,12 @@
 <x-app-layout>
-    <x-slot name="header">
+    <x-slot name="header" >
+        <div class="flex justify-between">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Posts') }}
+        </h2>
+            <button type="button" onclick=location.href="{{ route('posts.create') }}" 
+            class="btn btn-info font-bold text-blue-800">Create</button>
+        </div>
     </x-slot>
     <x-posts-list :posts="$posts" />
 </x-app-layout>
