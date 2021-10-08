f52623536b7279df199626bb806a90bcc6c42175
ChiKim22 <wlsgh2505@gmail.com>
2021-10-08 18:23:52 +0900

    Laravel (+Vue.js)

--- resources/views/components/posts-show.blade.php
+++ resources/views/components/posts-show.blade.php
@@ -8,6 +8,10 @@
         <div class="card-body">
           <h5 class="card-title">{{ $post->title }}</h5>
           <p class="card-text">{{ $post->content }}</p>
+          <div class="mt-10">
+            {{-- Like Button --}}
+            <like-button :post="{{ $post }}"/> 
+          </div>
         </div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">등록일 : {{ $post->created_at->diffForHumans() }}</li>
