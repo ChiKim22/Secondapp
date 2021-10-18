9a53248246fcd90f8d155fe3f8428ba198f69fea
ChiKim22 <wlsgh2505@gmail.com>
2021-10-18 15:40:00 +0900

    Laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -32,6 +33,40 @@
                 {{ $slot }}
             </main>
         </div>
+        <script>
+            @if (session('success'))
+                showSuccessMsg();
+            @endif
+
+            function confirmDelete(e) {
+              myForm = document.getElementById('form'); // form 이란 id 를 가진 dom 객체를 가져옴.
+              flag = confirm("Are You Sure?");
+    
+              if(flag){
+                //서버에 서브밋
+                myForm.submit();
+              }
+               //e.preventDefault(); // form이 서버로 전달되는 것을 막아준다.
+            }
+
+            function deleteImage(){
+                editForm = document.getElementById('editForm');
+                editForm._method.value = 'delete'; // method scopping
+                editForm.action = '/posts/image/' + id;
+                editForm.submit();
+            return false;
+            }
+
+            function showSuccessMsg(){
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'Your work has been saved.',
+                    showConfrimButton: false,
+                    timer: 1500
+                })
+            }
+          </script> 
     </body>
 </html>
  
